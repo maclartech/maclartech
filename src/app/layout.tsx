efb2adb09d50f069eb70659066b0de7c20b710ba
Maria Clara Carvalho <mclaracarvalho.tech@gmail.com>
2026-05-26 17:27:23 -0300

    feat: create initial portfolio structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Maria Clara Carvalho",
+  description: "Front-End Developer",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
--- src/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Maria Clara Carvalho</h1>
-      <p>Front-End Developer</p>
-    </main>
-  );
-}
